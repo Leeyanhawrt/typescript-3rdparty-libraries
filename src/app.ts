b515b5e260339a2f705ebbc500595df8bc41e735
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-23 16:31:25 -0400

    Add class validator

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import Product from "./product.model";
 import "reflect-metadata";
 import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 
 const products = [
   { title: "A Carpet", price: 29.99 },
   { title: "A Book", price: 10.99 },
 ];
+
+const newProduct = new Product("", -5.99);
+validate(newProduct).then((errors) => {
+  if (errors.length > 0) {
+    console.log(`VALIDATION ERRORS:`);
+    console.log(errors);
+  }
+});
+console.log(newProduct.getInformation());
 
 // const p1 = new Product("Sonos Arc", 1089);
 
